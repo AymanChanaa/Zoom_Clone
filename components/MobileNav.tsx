4119f590ddec52db8f67f05148a8450dfb7186f5
aimaneChanaa <aimane.chanaa@marketingconfort.com>
2025-08-17 13:04:09 +0100

    Create First component and integrate Login with Clerk

---
+++ components/MobileNav.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="Hamburger menu icon"
+            width={36}
+            height={36}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent
+          side="left"
+          className="border-none dark bg-[var(--dark-1)]"
+        >
+          <SheetHeader>
+            <SheetTitle asChild>
+              <Link href="/" className="flex items-center gap-2">
+                <Image
+                  src="/icons/logo.svg"
+                  alt="Yoom logo"
+                  width={32}
+                  height={32}
+                />
+                <p className="text-[26px] font-extrabold text-white">Yoom</p>
+              </Link>
+            </SheetTitle>
+          </SheetHeader>
+
+          <div className="h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto ml-3 mr-3">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-8 text-white">
+                {sidebarLinks.map((link) => {
+                  const isActive =
+                    pathname === link.route;
+                  return (
+                    <SheetClose asChild key={link.route}>
+                      <Link
+                        href={link.route}
+                        key={link.label}
+                        className={cn(
+                          "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
+                          {
+                            "bg-[var(--blue-1)]": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={link.imgUrl}
+                          alt={link.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">
+                          {link.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
